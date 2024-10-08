f6445f4705eae43b344b0b963ece14329521fe27
Tamas Horvath <horvath.tamas902@gmail.com>
2024-10-08 11:46:37 +0700

    wip

--- script/Deployment.s.sol
+++ script/Deployment.s.sol
@@ -6,7 +6,7 @@ import "../src/MyNFT.sol";
 
 contract DeployMyNFT is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("8c87bdb9ef563c4e780ae3e4b7f3c3e87eff6f60cb9fb565b57788c34efd0ab1");
         vm.startBroadcast(deployerPrivateKey);
 
         MyNFT myNft = new MyNFT();
--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
     uint256 public nextTokenId;
